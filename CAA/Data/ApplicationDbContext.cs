ea8cb076674476597a899f1aff4bd2a5329eb67e
Ryan Johnson <ryanjohnson4747@yahoo.co.uk>
2024-05-19 20:56:06 +0100

    Add project files.

---
+++ CAA/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CAA.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CAA.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        { 
+        }
+
+        public DbSet<LeadEntity> Leads { get; set; }
+    }
+}
---
+++ CAA/Models/LeadEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CAA.Models
+{
+    public class LeadEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime LeadDate { get; set; }
+        public string LeadSource { get; set; }
+        public string Name { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+
+
+    }
+}
